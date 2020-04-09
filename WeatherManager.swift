2dfa37f2647a86a91e6622c01655c79c696108b7
vaibhavbisht06 <vaibhavbisht06@gmail.com>
2020-04-09 21:03:59 +0530

    URL added

---
+++ WeatherManager.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Vaibhav Bisht on 09/04/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+struct WeatherManager {
+    let weatherURL = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22#"
+}
