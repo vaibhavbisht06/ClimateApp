cb1a8226e13868b0c85e9fff4eb870bd8057b2dd
Vaibhav Bisht <vaibhavbisht@Vaibhavs-MacBook-Air.local>
2020-04-14 17:58:34 +0530

    GetWeatherFunc Added

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -38,13 +38,31 @@ struct WeatherManager {
         let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             print(decodedData.name)
             print(decodedData.main.temp)
-//            let id = print(decodedData.weather[0].id)
+            let id = decodedData.weather[0].id
+            print(getConditionName(weatherId: id))
         }catch{
              print(error)
         }
     }
-//    func getConditionName(weaterId: Int) -> String{
-        
-//    }
+    func getConditionName(weatherId: Int) -> String{
+     switch weatherId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+    }
 }
 
